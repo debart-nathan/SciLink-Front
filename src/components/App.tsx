34083a84adbd258823c60ab68bab29e5dd098fa7
debart-nathan <denathan@laposte.net>
2023-10-05 14:43:02 +0200

    basic route

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,20 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import Header from './common/Header';
+import Footer from './common/Footer';
+import Home from './Home';
+
 function App() {
-    return <div></div>;
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}>
+
+        </Route>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  );
 }
 
 export default App;
