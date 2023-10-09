43ea256caf6490f0b1963b3702df0ae8b40789ce
ClaireM34 <146953434+ClaireM34@users.noreply.github.com>
2023-10-09 15:21:02 +0200

    modifier login

--- src/components/FormLogin.tsx
+++ src/components/FormLogin.tsx
@@ -1,33 +1,35 @@
 import { useState } from "react";
-
-const FormLogin = ({ onSubmitLoginUser, inputNameRef }) => {
-   const [username, setUsername] = useState("");
-  const [pwd, setPwd] = useState("");
+const FormLogin = ({ onSubmitLoginUser, inputNameRef}:{onSubmitLoginUser: any, inputNameRef:any}) => {
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+  
+   
     return (
       <form
+  
         onSubmit={(event) => {
           event.preventDefault();
           onSubmitLoginUser(event);
         }}
-        className="d-flex gap-2 w-50 align-items-center">
-        <label htmlFor="username-form-login-user" className="form-label ">UserName : </label>
-        <input ref={inputNameRef} name="username" value={username} onChange={(e) => setUsername(e.target.value)} type="text" id="username-form-login-user" className="form-control w-50" />
-        <label htmlFor="pwd-form-login-user" className="form-label ">Pwd : </label>
-        <input ref={inputNameRef} name="pwd" value={pwd} onChange={(e) => setPwd(e.target.value)} type="password" id="pwd-form-login-user"  className="form-control w-50" />
+        className="d-flex gap-2 w-50 align-items-center" action="/login">
+        <label htmlFor="Email-form-login-user" className="form-label ">Email : </label>
+        <input ref={inputNameRef} name="email" value={email} onChange={(e) => setEmail(e.target.value)} type="text" id="setEmail-form-login-user" className="form-control w-50" />
+        <label htmlFor="password-form-login-user" className="form-label ">Password: </label>
+        <input ref={inputNameRef} name="password" value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password-form-login-user"  className="form-control w-50" />
         <button onClick={() => { onSubmitLoginUser() }} className="btn btn-success" type="submit">Login</button>
       </form>
     );
-  }
-
+  
+      }
   export default FormLogin;
 
 
 //   <div class="form signin">
 //             <h2>sign in</h2>
 //             <div class="inputBox">
-//                 <input type="text" required="required">
+//                 <input type="email" required="required">
 //                 <i class="fa-regular fa-user"></i>
-//                 <span>username</span>
+//                 <span>email</span>
 //             </div>
 //             <div class="inputBox">
 //                 <input type="password" required="required">
