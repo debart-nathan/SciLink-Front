15f0b75c70c2646a91cc9b6d758cff5696c313d5
Benoit Gueheneux <benoit.gueheneux@hotmail.com>
2023-10-10 09:37:45 +0200

    add-interface

---
+++ src/components/detailPageResearchCenter/interfaces/InvestorInterface.tsx
@@ -0,0 +1,8 @@
+export default interface InvestorInterface {
+    id: number,
+    nom: string,
+    sigle: string,
+    nature: string,
+    label: string,
+    user_id: number
+}
---
+++ src/components/detailPageResearchCenter/interfaces/ResearcherInterface.tsx
@@ -0,0 +1,4 @@
+export default interface ResearcherInterface {
+    id : number,
+    user_id : number,
+}
