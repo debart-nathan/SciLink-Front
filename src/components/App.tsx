f73c1b0d3bcf4c17d36315a55d477a39b917f479
Benoit Gueheneux <benoit.gueheneux@hotmail.com>
2023-10-10 10:50:30 +0200

    add-routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,12 +5,18 @@ import Home from "./Home";
 import SearchPage from "./SearchPage";
 import ContactUs from './common/Footers/ContactUs';
 import Mentions from './common/Footers/Mentions';
+import PageDetailInvestor from "./detailPageInvestor/PageDetailInvestor";
+import PageDetailResearchCenter from "./detailPageResearchCenter/PageDetailResearchCenter";
+import PageDetailUser from "./detailPageUser/PageDetailUser";
 
 function App() {
     return (
         <BrowserRouter>
             <Header />
             <Routes>
+                <Route path="/user/:id" element={<PageDetailUser />} />
+                <Route path="/researchCenter/:id" element={<PageDetailResearchCenter />} />
+                <Route path="/investor/:id" element={<PageDetailInvestor />} />
                 <Route path="/recherche" element={<SearchPage />}></Route>
                 <Route path="/contact-us" element={<ContactUs />} />
                 <Route path="/Mentions" element={<Mentions />} />
