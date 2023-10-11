f9582eb75b309544f11afd9bf0cd701b909cb8dc
debart-nathan <denathan@laposte.net>
2023-10-11 14:57:28 +0200

    formating

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,8 +3,8 @@ import Header from "./common/Headers/Header";
 import Footer from "./common/Footers/Footer";
 import Home from "./Home";
 import SearchPage from "./SearchPage";
-import ContactUs from './common/Footers/ContactUs';
-import Mentions from './common/Footers/Mentions';
+import ContactUs from "./common/Footers/ContactUs";
+import Mentions from "./common/Footers/Mentions";
 import PageDetailInvestor from "./detailPageInvestor/PageDetailInvestor";
 import PageDetailResearchCenter from "./detailPageResearchCenter/PageDetailResearchCenter";
 import PageDetailUser from "./detailPageUser/PageDetailUser";
@@ -13,28 +13,32 @@ import Connexion from "./Connexion/Connexion";
 
 
 function App() {
+    return (
+        <BrowserRouter>
+            <Header />
+            <div className="container">
+                <Routes>
+                    <Route path="/login" element={<Connexion />} />
+                    <Route path="/user/:id" element={<PageDetailUser />} />
+                    <Route
+                        path="/researchCenter/:id"
+                        element={<PageDetailResearchCenter />}
+                    />
+                    <Route
+                        path="/investor/:id"
+                        element={<PageDetailInvestor />}
+                    />
+                    <Route path="/recherche" element={<SearchPage />}></Route>
+                    <Route path="/contact-us" element={<ContactUs />} />
+                    <Route path="/Mentions" element={<Mentions />} />
+                    <Route path="/admin" element={<AdminPage />}></Route>
+                    <Route path="/" element={<Home />}></Route>
+                </Routes>
+            </div>
 
-  return (
-    <BrowserRouter>
-      <Header />
-      <div className="container">
-        <Routes>
-                <Route path="/login" element={<Connexion />} />
-                <Route path="/user/:id" element={<PageDetailUser />} />
-                <Route path="/researchCenter/:id" element={<PageDetailResearchCenter />} />
-                <Route path="/investor/:id" element={<PageDetailInvestor />} />
-                <Route path="/recherche" element={<SearchPage />}></Route>
-                <Route path="/contact-us" element={<ContactUs />} />
-                <Route path="/Mentions" element={<Mentions />} />
-                <Route path="/admin" element={<AdminPage />}></Route>
-                <Route path="/" element={<Home />}></Route>
-                
-            </Routes>
-        </div>
-          <Footer/>
+            <Footer />
         </BrowserRouter>
-   );
-
+    );
 }
 
 export default App;
