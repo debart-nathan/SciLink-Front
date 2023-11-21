99026fb2f084b931be41367889629e881a2bb987
debart-nathan <denathan@laposte.net>
2023-11-22 00:19:00 +0100

    protect user

--- src/components/detailPageUser/InvestorLinks.tsx
+++ src/components/detailPageUser/InvestorLinks.tsx
@@ -4,46 +4,58 @@ import InvestorInterface from "../../interfaces/InvestorInterface";
 import CreateProfileInvestor from "./CreateProfileInvestor";
 
 const InvestorLinks = ({ id }: { id: string }) => {
-  const [InvestorsState, setInvestorsState] = useState<InvestorInterface[]>();
-  useEffect(() => {
-    InvestorSelect("Investors", "app_user", id);
-  }, []);
+    const [InvestorsState, setInvestorsState] = useState<InvestorInterface[]>();
+    const [isConnectedUser, setIsConnectedUser] = useState(false);
 
-  async function InvestorSelect(
-    entityName: string,
-    conditionName: string,
-    condition: string
-  ) {
-    try {
-      const response = await JsonServerB.EntitySelectWCondition(
-        entityName,
-        conditionName,
-        condition
-      );
-      setInvestorsState(response);
-    } catch (error) {
-      console.error(`Erreur attrapée dans ${entityName}Select : ` + error);
+    useEffect(() => {
+        InvestorSelect("Investors", "app_user", id);
+    }, []);
+
+    useEffect(() => {
+        const checkUser = async () => {
+            const isConnected = await JsonServerB.IsConnectedUser(id);
+            setIsConnectedUser(isConnected);
+        };
+
+        checkUser();
+    }, [id]);
+
+    async function InvestorSelect(
+        entityName: string,
+        conditionName: string,
+        condition: string
+    ) {
+        try {
+            const response = await JsonServerB.EntitySelectWCondition(
+                entityName,
+                conditionName,
+                condition
+            );
+            setInvestorsState(response);
+        } catch (error) {
+            console.error(
+                `Erreur attrapée dans ${entityName}Select : ` + error
+            );
+        }
     }
-  }
 
-  return (
-    <>
-    <CreateProfileInvestor userId={id}/>
-      {InvestorsState ? (
-        <div className="row border border-danger border-bottom-0 mt-2">
-          {InvestorsState.map((Investors: any) => (
-            <a
-              className="col-12 col-md-6 text-danger"
-              key={Investors.id}
-              href={`/investor/${Investors.id}`}
-            >
-              Lien vers {Investors.name}
-            </a>
-          ))}
-        </div>
-      ) : null}
-    </>
-  );
+    return (
+        <>
+            {isConnectedUser && <CreateProfileInvestor userId={id} />}
+            {InvestorsState ? (
+                <div className="row border border-danger border-bottom-0 mt-2">
+                    {InvestorsState.map((Investors: any) => (
+                        <a
+                            className="col-12 col-md-6 text-danger"
+                            key={Investors.id}
+                            href={`/investor/${Investors.id}`}>
+                            Lien vers {Investors.name}
+                        </a>
+                    ))}
+                </div>
+            ) : null}
+        </>
+    );
 };
 
 export default InvestorLinks;
