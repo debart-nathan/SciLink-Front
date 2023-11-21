addd089719f0f3cd794f5e22f3068b2a4c9c6527
debart-nathan <denathan@laposte.net>
2023-11-21 11:37:20 +0100

    fix-recherche
    ajouter fixe url filtre
    
    centraliser url

--- src/components/filter/SearchFilterInvestor.tsx
+++ src/components/filter/SearchFilterInvestor.tsx
@@ -1,69 +1,89 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FieldValues, useFormContext } from "react-hook-form";
+import JsonServerB from "../../services/jsonServerB";
 import Button from "react-bootstrap/Button";
 import Popover from "react-bootstrap/Popover";
 import Overlay from "react-bootstrap/Overlay";
 
 interface SearchFilterInvestorProps {
-  resetInvestorFields: () => FieldValues;
+    resetInvestorFields: () => FieldValues;
 }
 
 const SearchFilterInvestor: React.FC<SearchFilterInvestorProps> = ({
-  resetInvestorFields,
+    resetInvestorFields,
 }) => {
-  const { register, reset } = useFormContext();
-  //TODO get from server
-  const domains: { id: number; name: string }[] = [];
+    const { register, reset } = useFormContext();
 
-  const [show, setShow] = useState(false);
-  const target = useRef(null);
+    const [domains, setDomains] = useState<{ id: number; name: string }[]>([]);
 
-  return (
-    <>
-      <Button
-        className="btnx "
-        ref={target}
-        onClick={() => setShow(!show)}
-      ><span></span><span></span><span></span><span></span>
-        Filtres
-      </Button>
+    const [show, setShow] = useState(false);
+    const target = useRef(null);
 
-      <Overlay target={target.current} show={show} placement="bottom">
-        {(props) => (
-          <Popover className="p-3" id="popover-contained" {...props}>
-            <label className="form-label">Domaine :</label>
-            <select
-              className="form-control"
-              id="investor-domain"
-              {...register("investor.domain")}
-            >
-              <option className="form-check" value="">
-                choisissez une Option
-              </option>
-              {domains.map((domain) => {
-                return (
-                  <option className="form-check" value={domain.id}>
-                    {domain.name}
-                  </option>
-                );
-              })}
-            </select>
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await JsonServerB.EntitySelectAll("Domains");
+            setDomains(result);
+        };
+        fetchData();
+    }, []);
 
-            <button
-              className="btnx"
-              onClick={(ev) => {
-                ev.preventDefault();
-                reset(resetInvestorFields());
-              }}
-            >
-              <span></span><span></span><span></span><span></span>
-              Reset les Filtres
-            </button>
-          </Popover>
-        )}
-      </Overlay>
-    </>
-  );
+    return (
+        <>
+            <Button
+                className="btnx "
+                ref={target}
+                onClick={() => setShow(!show)}
+                disabled>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Filtres
+            </Button>
+
+            <Overlay target={target.current} show={show} placement="bottom">
+                {(props) => (
+                    <Popover className="p-3" id="popover-contained" {...props}>
+                        <label className="form-label">Domaine :</label>
+                        <select
+                            className="form-control"
+                            id="investor-domain"
+                            {...register("investor.domain")}>
+                            <option className="form-check" value="">
+                                choisissez une Option
+                            </option>
+                            {domains.map((domain) => {
+                                if (domain && domain.id && domain.name) {
+                                    return (
+                                        <option
+                                            key={domain.id}
+                                            className="form-check"
+                                            value={domain.id}>
+                                            {domain.name}
+                                        </option>
+                                    );
+                                }
+                                return null;
+                            })}
+                        </select>
+
+                        <button
+                            className="btnx"
+                            onClick={(ev) => {
+                                ev.preventDefault();
+                                reset(resetInvestorFields());
+                            }}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            Reset les Filtres
+                        </button>
+                    </Popover>
+                )}
+            </Overlay>
+        </>
+    );
 };
 
 export default SearchFilterInvestor;
